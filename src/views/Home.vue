33bb6b1cbaf22a83ac1dc581c8c626c3fb5d62a7
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-07-28 13:13:28 +0200

    Part 5

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Form />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Form from "@/components/Form.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Form
+  }
+};
+</script>
