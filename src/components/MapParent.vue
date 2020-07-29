efadbabfe0c8ec82352686992275141382ef2893
Alexander Rind <1217527+alex-rind@users.noreply.github.com>
2020-07-29 10:10:48 +0200

    NWK work in progress

---
+++ src/components/MapParent.vue
@@ -0,0 +1,57 @@
+<template>
+	<div>
+		<ul>
+			<li v-for="cur in contacts" :key="cur.id">
+				<b>{{ cur.id }}</b> {{ cur.angle }}, {{ cur.distance }}
+			</li>
+			<button @click="addRandomContact()">Add contact</button>
+		</ul>
+		<svg width="600" height="600">
+			<circle r="5" cx="23" cy="7" />
+		</svg>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+interface Contact {
+	id: string;
+	angle: number;
+	distance: number;
+}
+
+@Component
+export default class MapParent extends Vue {
+	private ego: string;
+	private contacts: Array<Contact>;
+
+	constructor() {
+		super();
+		this.ego = "Alex";
+		this.contacts = [
+			{ id: "Max", angle: 30, distance: 20 },
+			{ id: "Klaus", angle: 150, distance: 90 },
+			{ id: "Julia", angle: 350, distance: 50 },
+		];
+	}
+
+	addRandomContact() {
+		this.contacts.push({
+			id: "Neu",
+			angle: Math.round(Math.random() * 360),
+			distance: Math.round(Math.random() * 100),
+		});
+	}
+}
+</script>
+
+<style>
+ul {
+	width: 200px;
+}
+svg {
+	background: aliceblue;
+	border: 1px solid red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Form />
+    <MapParent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Form from "@/components/Form.vue";
+import MapParent from "@/components/MapParent.vue";
 
 export default {
   name: "Home",
   components: {
-    Form
+    Form,
+    MapParent
   }
 };
 </script>
