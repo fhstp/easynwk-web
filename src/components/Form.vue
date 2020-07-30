feb6d3ea0f87100db08bf9b9e5824d14bec313f7
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-07-30 11:44:52 +0200

    The list of alter is modularized now

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -10,30 +10,9 @@
         v-if="!addAlterPageVisible"
         v-on:submit="submit()"
         v-on:editEgo="editEgo"
-        v-bind:key="componentKey"
+        v-bind:key="egoKey"
       />
-
-      <div id="alterFields" v-if="egoSubmitted && !addAlterPageVisible">
-        <div id="alterListTitle">
-          <h2 id="contactListTitleHeading" class="inlineElement textLeft alterListFont">Kontakte:</h2>
-          <img
-            id="addAlterIcon"
-            class="iconRight"
-            src="../assets/addAlter.svg"
-            alt="Kontakt hinzufügen"
-            @click="addAlter()"
-          />
-        </div>
-        <div id="listOfAlter">
-          <ul>
-            <li v-bind:key="alterN.id" v-for="alterN in alter">
-              <div>
-                <h2 class="alterListFont">{{ alterN.name }}/{{ alterN.role }}</h2>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Alter v-if="egoSubmitted" />
       <!-- <div v-if="addAlterPageVisible">
         <img
           class="iconLeft"
@@ -60,96 +39,34 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { v4 as uuid } from "uuid";
 import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 import Ego from "./Ego/components/Ego.vue";
-import EgoTitle from "./Ego/components/Ego.vue";
+import Alter from "./Alter/components/Alter.vue";
 
 Vue.component("v-select", vSelect);
 
 Vue.component("Ego", Ego);
-
-interface Alter {
-  id: string;
-  name: string;
-  gender: Array<string>;
-  role: string;
-  note: string;
-  amountOfEdges: number;
-}
+Vue.component("Alter", Alter);
 
 @Component
 export default class Form extends Vue {
-  //Global
-  @Prop() private gender!: Array<string>;
-  //Ego-related
   @Prop(Boolean) private egoSubmitted!: boolean;
-  @Prop() private componentKey!: number;
-  // Alter-related
-  @Prop(Array) private alter!: Array<Alter>;
-  @Prop(Array) private roles!: Array<string>;
-  @Prop(Array) private amountOfEdges!: Array<number>;
+  @Prop() private egoKey!: number;
   @Prop(Boolean) private addAlterPageVisible?: boolean = false;
-  @Prop() private unsavedAlter!: Alter;
 
   constructor() {
     super();
-
-    this.componentKey = 0;
+    this.egoKey = 0;
     this.egoSubmitted = false;
-    this.roles = ["Fr", "Fa", "Ko", "He"];
-    this.amountOfEdges = [0, 1, 2];
-
-    // "alter" initialization
-    this.alter = [
-      {
-        id: uuid(),
-        name: "Alter 1",
-        gender: this.gender,
-        role: this.roles[0],
-        note: "Alter note",
-        amountOfEdges: 2
-      },
-      {
-        id: uuid(),
-        name: "Alter 2",
-        gender: this.gender,
-        role: this.roles[0],
-        note: "Alter note",
-        amountOfEdges: 1
-      },
-      {
-        id: uuid(),
-        name: "Alter 3",
-        gender: this.gender,
-        role: this.roles[1],
-        note: "My new note",
-        amountOfEdges: 0
-      }
-    ];
   }
   submit() {
     this.egoSubmitted = true;
   }
   editEgo() {
-    this.componentKey += 1;
+    this.egoKey += 1;
 
     this.egoSubmitted = false;
-  }
-  addAlter() {
-    this.addAlterPageVisible = true;
-    this.unsavedAlter = {
-      id: uuid(),
-      name: "",
-      gender: this.gender,
-      role: "",
-      note: "",
-      amountOfEdges: 0
-    };
-  }
-  closeCreateNewAlter() {
-    this.addAlterPageVisible = false;
   }
 }
 </script>
@@ -176,23 +93,8 @@ export default class Form extends Vue {
   color: grey;
   padding-left: 15px;
 }
-#listOfAlter {
-  margin-top: 50px;
-}
-
-#contactListTitleHeading {
-  margin: 0px;
-  padding: 5px;
-  padding-left: 10px;
-}
 
 #alterListTitle {
   padding: 5px;
-}
-#addAlterIcon {
-  padding: 5px;
-}
-.alterListFont {
-  font-size: 24px;
 }
 </style>
