c02303242c0ed6a361da50e928a68c49c801f531
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-03 09:18:23 +0200

    Replacing the page "Alter.vue"

---
+++ src/components/Alter/components/AddAlter.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <img
+      class="iconLeft"
+      src="../assets/addNewAlter.svg"
+      alt="Schließen"
+      @click="addAlter()"
+    />
+    <p>Name:</p>
+    <input type="text" v-model="unsavedAlter.name" />
+    <p>Beziehungstyp:</p>
+    <v-select v-model="unsavedAlter.role" v-bind:options="roles"></v-select>
+    <p>Geschlecht:</p>
+    <v-select
+      v-model="unsavedAlter.currentGender"
+      v-bind:options="gender"
+    ></v-select>
+    <p>Alter:</p>
+    <input type="text" v-model="unsavedAlter.age" />
+    <p>Notiz:</p>
+    <input type="text" class="note" v-model="unsavedAlter.note" />
+
+    <button @click="addAlter()">Fertig</button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import AlterInterface from ".//AlterInterface.vue";
+import { v4 as uuid } from "uuid";
+
+export default class AddAlter extends Vue {
+  @Prop() private unsavedAlter!: AlterInterface;
+  @Prop() private gender!: Array<string>;
+  constructor() {
+    super();
+    this.unsavedAlter = {
+      id: uuid(),
+      name: "",
+      gender: this.gender,
+      role: "",
+      note: "",
+      amountOfEdges: 0
+    };
+  }
+  addAlter() {
+    // this.alter[this.alter.length] = this.unsavedAlter;
+    // localStorage.setItem("alter", JSON.stringify(this.alter));
+    console.log("addAlter");
+  }
+}
+</script>
---
+++ src/components/Alter/components/AlterList.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div id="alterFields">
+      <h2
+        id="contactListTitleHeading"
+        class="inlineElement textLeft alterListFont"
+      >
+        Kontakte:
+      </h2>
+
+      <img
+        id="addAlterIcon"
+        class="iconRight"
+        src="../assets/openAddAlterPage.svg"
+        alt="Kontakt hinzufügen"
+        @click="openAddAlterPage()"
+      />
+      <div id="listOfAlter">
+        <ul>
+          <li v-bind:key="alterN.id" v-for="alterN in alter">
+            <div>
+              <h2 class="alterListFont">{{ alterN.name }}/{{ alterN.role }}</h2>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import AlterInterface from "./AlterInterface.vue";
+import { v4 as uuid } from "uuid";
+
+export default class AlterList extends Vue {
+  @Prop(Array) private alter!: Array<AlterInterface>;
+  @Prop() private gender!: Array<string>;
+  @Prop(Array) private roles!: Array<string>;
+
+  @Prop(Array) private amountOfEdges!: Array<number>;
+
+  constructor() {
+    super();
+    this.roles = ["Fr", "Fa", "Ko", "He"];
+    this.gender = ["männlich", "weiblich", "divers"];
+    this.amountOfEdges = [0, 1, 2];
+
+    this.alter = [];
+    if (localStorage.alter) {
+      const bufferArray = JSON.parse(localStorage.alter);
+      for (let i = 0; i < bufferArray.length; i++) {
+        this.alter[i] = bufferArray[i];
+      }
+    }
+  }
+
+  openAddAlterPage() {
+    this.$emit("openAddAlterPage");
+  }
+}
+</script>
+<style scoped>
+#contactListTitleHeading {
+  margin: 0px;
+  padding: 10px;
+  padding-left: 10px;
+  padding-top: 10px;
+}
+.alterListFont {
+  font-size: 24px;
+}
+#addAlterIcon {
+  padding: 15px;
+  padding-right: 17px;
+}
+</style>
