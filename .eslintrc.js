7cce9b70a18974a70fac970d655a755d952aecbf
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-03 09:37:54 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,7 @@ module.exports = {
     "@vue/prettier/@typescript-eslint"
   ],
   parserOptions: {
-    ecmaVersion: 2020,
-    // ecmaFeatures: {
-    //   legacyDecorators: true
-    // },
-    parser: "babel-eslint"
+    ecmaVersion: 2020
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
