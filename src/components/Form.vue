ba8f97a1d72ca5f43a69d609e607fdd8f2dbc4df
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-04 09:58:34 +0200

    Update Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,116 +1,111 @@
 <template>
-  <div>
-    <div id="form">
-      <div id="title">
-        <div id="AddAlterTitle" v-if="addAlterPageVisible">
-          <h2>Neuer Kontakt: Erstellen</h2>
-        </div>
-      </div>
-      <!-- Through binding this dynamic class you can avoid having the vue-component reload itself and avoid having its variables be resetted -->
-      <Ego
-        v-bind:class="{ noDisplay: addAlterPageVisible }"
-        v-on:submit="submit()"
-        v-on:editEgo="editEgo"
-        v-bind:key="egoKey"
-      />
-      <AlterList
-        v-on:openAddAlterPage="openAddAlterPage"
-        v-if="egoSubmitted && !addAlterPageVisible"
-        v-bind:alterarray="alter"
-      />
-      <AddAlter
-        v-if="egoSubmitted && addAlterPageVisible"
-        @add-alter="(a) => alter.push(a)"
-        v-on:openAlterListPage="openAlterListPage"
-      />
+  <div id="app">
+    <div id="nav">
+      <!-- <router-link to="/">Home</router-link> -->
+      <!-- <router-link to="/about">About</router-link> -->
     </div>
+    <router-view />
   </div>
 </template>
 
-<script lang="ts">
-import { Watch, Component, Prop, Vue } from "vue-property-decorator";
-import vSelect from "vue-select";
-import "vue-select/dist/vue-select.css";
-import Ego from "./Ego/components/Ego.vue";
-import AlterList from "./Alter/components/AlterList.vue";
-import AddAlter from "./Alter/components/AddAlter.vue";
-import Alter from "@/components/Alter/components/Alter";
-
-Vue.component("v-select", vSelect);
-
-Vue.component("Ego", Ego);
-Vue.component("AlterList", AlterList);
-Vue.component("AddAlter", AddAlter);
-
-@Component
-export default class Form extends Vue {
-  private egoSubmitted!: boolean;
-  private egoKey!: number;
-  // private alterListKey!: number;
-  private addAlterPageVisible?: boolean = false;
-  private egoTitleV2Activated?: boolean = false;
-
-  private alter = [] as Alter[];
-
-  @Watch("alter")
-  onAlterChange(newAlter: any) {
-    localStorage.alter = JSON.stringify(newAlter);
-  }
-
-  constructor() {
-    super();
-    if (localStorage.alter) {
-      this.alter = JSON.parse(localStorage.alter);
-    }
-    this.egoKey = 0;
-
-    this.egoSubmitted = false;
-  }
-
-  submit() {
-    this.egoSubmitted = true;
-  }
-  editEgo() {
-    this.egoKey += 1;
-
-    this.egoSubmitted = false;
-  }
-  openAddAlterPage() {
-    this.addAlterPageVisible = true;
-  }
-  openAlterListPage() {
-    this.addAlterPageVisible = false;
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+#nav {
+  padding: 30px;
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-#form {
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+button {
+  font-size: 36px;
+  margin: 10px;
+  padding: 5px;
+  background-color: white;
+  border: none;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+li {
   display: inline-block;
-  height: 609px; /*From the Figa´ma-file*/
-  width: 548px; /*From the Figa´ma-file*/
-  border-style: solid;
-  border-width: 4px;
-  border-color: black;
-  margin: 5 auto;
-  padding: 0px;
+  margin: 0;
+  padding-left: 10px;
+  background-color: white;
+  width: 98%;
+  text-align: left;
+  margin-bottom: 5px;
+}
+
+a {
+  color: #42b983;
+}
+input,
+textarea {
+  height: 25px;
+  width: 280px;
+  margin: 0 auto;
+  vertical-align: middle;
+  border: none;
+  font-size: 18px;
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 2px;
+  margin-right: 10px;
+}
+textarea {
+  min-width: 85%;
+  max-width: 85%;
+  min-height: 25px;
+  max-height: 210px;
+}
+
+p {
+  font-size: 18px;
+  text-align: center;
   background-color: #c4c4c4;
 }
 
-#title {
-  text-align: left;
+h1 {
+  margin: 0 auto;
+  font-size: 36px;
 }
-
-#alterTitle {
-  color: grey;
-  padding-left: 15px;
+.note {
+  height: 50px;
+  width: 85%;
 }
-
-#alterListTitle {
-  padding: 5px;
+.inlineElement {
+  display: inline-block;
 }
-.noDisplay {
-  display: none;
+.inputOrDropdownRight {
+  float: right;
+}
+.iconLeft {
+  float: left;
+  margin-left: 15px;
+}
+.textLeft {
+  float: left;
+  margin-left: 15px;
+}
+.textRight {
+  float: right;
+}
+.iconRight {
+  float: right;
+  padding: 10px;
 }
 </style>
