ef4131de55dbeb6e328b096623f07fbfeefae45a
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-04 09:50:19 +0200

    Update Form.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,150 +2,83 @@
   <div>
     <div id="form">
       <div id="title">
-        <div id="alterTitle" v-if="addAlterPageVisible">
+        <div id="AddAlterTitle" v-if="addAlterPageVisible">
           <h2>Neuer Kontakt: Erstellen</h2>
         </div>
       </div>
-      <Ego v-if="!addAlterPageVisible" v-on:submit="submit()" />
-
-      <!-- <div id="alterFields" v-if="egoSubmitted && !addAlterPageVisible">
-        <div id="alterListTitle">
-          <h2 id="contactListTitleHeading" class="inlineElement textLeft alterListFont">Kontakte:</h2>
-          <img
-            id="addAlterIcon"
-            class="iconRight"
-            src="../assets/addAlter.svg"
-            alt="Kontakt hinzufügen"
-            @click="addAlter()"
-          />
-        </div>
-        <div id="listOfAlter">
-          <ul>
-            <li v-bind:key="alter.id" v-for="alter in alter">
-              <div>
-                <h2 class="alterListFont">{{ alter.name }}/{{ alter.role }}</h2>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div> -->
-      <div v-if="addAlterPageVisible">
-        <img
-          class="iconLeft"
-          src="../assets/closeCreateNewAlter.svg"
-          alt="Schließen"
-          @click="closeCreateNewAlter()"
-        />
-        <p>Name:</p>
-        <input type="text" v-model="unsavedAlter.name" />
-        <!-- In the Figma-file there was: "Anonymes Zeichen" -->
-        <p>Beziehungstyp:</p>
-        <v-select v-model="unsavedAlter.role" v-bind:options="roles"></v-select>
-        <p>Geschlecht:</p>
-        <v-select v-model="unsavedAlter.currentGender"
-          v-bind:options="gender"></v-select>
-        <p>Alter:</p>
-        <input type="text" v-model="unsavedAlter.age" />
-        <p>Notiz:</p>
-        <input type="text" class="note" v-model="unsavedAlter.note" />
-        <!-- This following is a seperating line -->
-
-        <button>Fertig</button>
-      </div>
+      <!-- Through binding this dynamic class you can avoid having the vue-component reload itself and avoid having its variables be resetted -->
+      <Ego
+        v-bind:class="{ noDisplay: addAlterPageVisible }"
+        v-on:submit="submit()"
+        v-on:editEgo="editEgo"
+        v-bind:key="egoKey"
+      />
+      <AlterList
+        v-on:openAddAlterPage="openAddAlterPage"
+        v-if="egoSubmitted && !addAlterPageVisible"
+        v-bind:alterarray="alter"
+      />
+      <AddAlter
+        v-if="egoSubmitted && addAlterPageVisible"
+        @add-alter="(a) => alter.push(a)"
+        v-on:openAlterListPage="openAlterListPage"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import { v4 as uuid } from "uuid";
+import { Watch, Component, Prop, Vue } from "vue-property-decorator";
 import vSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 import Ego from "./Ego/components/Ego.vue";
+import AlterList from "./Alter/components/AlterList.vue";
+import AddAlter from "./Alter/components/AddAlter.vue";
+import Alter from "@/components/Alter/components/Alter";
 
 Vue.component("v-select", vSelect);
 
 Vue.component("Ego", Ego);
-
-interface Alter {
-  id: string;
-  name: string;
-  gender: Array<string>;
-  role: string;
-  note: string;
-  amountOfEdges: number;
-}
+Vue.component("AlterList", AlterList);
+Vue.component("AddAlter", AddAlter);
 
 @Component
 export default class Form extends Vue {
-  //Global
-  @Prop() private gender!: Array<string>;
-  @Prop(Boolean) private egoSubmitted?: boolean = false;
-  // Alter-related
-  @Prop(Array) private alter!: Array<Alter>;
-  @Prop(Array) private roles!: Array<string>;
-  @Prop(Array) private amountOfEdges!: Array<number>;
-  @Prop(Boolean) private addAlterPageVisible?: boolean = false;
-  @Prop() private unsavedAlter!: Alter;
+  private egoSubmitted!: boolean;
+  private egoKey!: number;
+  // private alterListKey!: number;
+  private addAlterPageVisible?: boolean = false;
+  private egoTitleV2Activated?: boolean = false;
 
-  /* // This watcher doesn't work. He would be for hecking ego.name for symbols and removing them
-  @Watch("ego.name", { immediate: true, deep: true })
-  nameField(newValue: string) {
-    const re = /[^A-Z0-9]/gi;
-    this.$set(this, this.ego.name, newValue.replace(re, ""));
+  private alter = [] as Alter[];
+
+  @Watch("alter")
+  onAlterChange(newAlter: any) {
+    localStorage.alter = JSON.stringify(newAlter);
   }
-*/
+
   constructor() {
     super();
-    this.roles = ["Fr", "Fa", "Ko", "He"];
-    this.amountOfEdges = [0, 1, 2];
+    if (localStorage.alter) {
+      this.alter = JSON.parse(localStorage.alter);
+    }
+    this.egoKey = 0;
 
-    // "alter" initialization
-    this.alter = [
-      {
-        id: uuid(),
-        name: "Alter 1",
-        gender: this.gender,
-        role: this.roles[0],
-        note: "Alter note",
-        amountOfEdges: 2
-      },
-      {
-        id: uuid(),
-        name: "Alter 2",
-        gender: this.gender,
-        role: this.roles[0],
-        note: "Alter note",
-        amountOfEdges: 1
-      },
-      {
-        id: uuid(),
-        name: "Alter 3",
-        gender: this.gender,
-        role: this.roles[1],
-        note: "My new note",
-        amountOfEdges: 0
-      }
-    ];
+    this.egoSubmitted = false;
   }
+
   submit() {
     this.egoSubmitted = true;
   }
   editEgo() {
+    this.egoKey += 1;
+
     this.egoSubmitted = false;
   }
-  addAlter() {
+  openAddAlterPage() {
     this.addAlterPageVisible = true;
-    this.unsavedAlter = {
-      id: uuid(),
-      name: "",
-      gender: this.gender,
-      role: "",
-      note: "",
-      amountOfEdges: 0
-    };
   }
-  closeCreateNewAlter() {
+  openAlterListPage() {
     this.addAlterPageVisible = false;
   }
 }
@@ -173,23 +106,11 @@ export default class Form extends Vue {
   color: grey;
   padding-left: 15px;
 }
-#listOfAlter {
-  margin-top: 50px;
-}
-
-#contactListTitleHeading {
-  margin: 0px;
-  padding: 5px;
-  padding-left: 10px;
-}
 
 #alterListTitle {
   padding: 5px;
 }
-#addAlterIcon {
-  padding: 5px;
-}
-.alterListFont {
-  font-size: 24px;
+.noDisplay {
+  display: none;
 }
 </style>
