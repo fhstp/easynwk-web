366831cb74e1175f84bce7c868a864efbaa9dc0a
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-05 16:31:58 +0200

    Update Alter.ts

--- src/components/Alter/components/Alter.ts
+++ src/components/Alter/components/Alter.ts
@@ -1,8 +1,8 @@
 import { v4 as uuid } from "uuid";
 
 export class Alter {
-  private _id!: string;
-  private _name!: string;
+  id!: string;
+  name!: string;
   role?: string = "";
   currentGender?: string = "";
   note?: string = "";
@@ -11,25 +11,9 @@ export class Alter {
   distance = 0;
 
   constructor(name: string, $angle: number, $distance: number) {
-    this._id = uuid();
-    this._name = name;
+    this.id = uuid();
+    this.name = name;
     this.angle = $angle;
     this.distance = $distance;
-  }
-
-  /**
-   * Getter $name
-   * @return {string}
-   */
-  public get name(): string {
-    return this._name;
-  }
-
-  /**
-   * Setter $name
-   * @param {string} value
-   */
-  public set name(value: string) {
-    this._name = value;
   }
 }
