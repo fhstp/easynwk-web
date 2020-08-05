dbb30b3d51f8c4ba20891773a8d7d13fd881eab9
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-05 14:23:10 +0200

    Merge-fix

--- src/components/RepositionOverlay.vue
+++ src/components/RepositionOverlay.vue
@@ -1,8 +1,3 @@
 <script lang="ts">
 // make a svg exactly over the NWK with transparent notification text that accepts a touch as the new position 
 </script>
-=======
-<script lang="ts">
-// make a svg exactly over the NWK with transparent notification text that accepts a touch as the new position 
-</script>
-
