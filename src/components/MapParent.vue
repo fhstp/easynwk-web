6e4c338e30f1d59afe4dcb942baa8867813b860d
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-05 10:06:40 +0200

    Add files via upload

---
+++ src/components/MapParent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="cur in alteri" :key="cur.id">
+        <b>{{ cur.name }}</b>
+        {{ cur.angle }}, {{ cur.distance }}
+      </li>
+      <button @click="addRandomContact()">Add contact</button>
+    </ul>
+    <NetworkMap v-bind:alteri="alteri" @map-click="mapclick" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import NetworkMap from "@/components/NetworkMap.vue";
+
+import { Alter2 } from "@/data/Alter.ts";
+
+@Component({
+  components: {
+    NetworkMap,
+  },
+})
+export default class MapParent extends Vue {
+  private ego: string;
+  private alteri: Array<Alter2>;
+
+  constructor() {
+    super();
+    this.ego = "Alex";
+    this.alteri = [
+      new Alter2("Max", 30, 20),
+      new Alter2("Klaus", 150, 90),
+      new Alter2("Julia", -10, 50),
+    ];
+  }
+
+  addRandomContact() {
+    this.alteri.push(
+      new Alter2(
+        "Neu",
+        Math.round(Math.random() * 360 - 180),
+        Math.round(Math.random() * 100)
+      )
+    );
+  }
+
+  mapclick(coords: { distance: number; angle: number }) {
+    console.log("map click in parent comp: ");
+    this.alteri.push(new Alter2("heya", coords.angle, coords.distance));
+    console.log(coords);
+  }
+}
+</script>
+
+<style>
+ul {
+  width: 200px;
+}
+svg {
+  background: aliceblue;
+  border: 1px solid red;
+}
+</style>
