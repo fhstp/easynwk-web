06152d8e12beb65d8834fb7f06622fdc45c03b1c
Eduard Kumpan <68686802+EKumpan@users.noreply.github.com>
2020-08-05 16:29:27 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="home">
-    <Form />
-    <MapParent />
+    <Form v-bind:key="formKey" />
+    <MapParent v-on:reloadForm="reloadForm" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
 import Form from "@/components/Form.vue";
 import MapParent from "@/components/MapParent.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
 
-export default {
-  name: "Home",
-  components: {
-    Form,
-    MapParent
+Vue.component("Form", Form);
+Vue.component("MapParent", MapParent);
+
+@Component
+export default class Home extends Vue {
+  private formKey!: number;
+
+  constructor() {
+    super();
+    this.formKey = 0;
   }
-};
+
+  reloadForm() {
+    this.formKey++;
+  }
+}
 </script>
