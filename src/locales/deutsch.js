55faf9b5f91398839fc8733f66513542eaf6f02a
julia221501 <sakura.ju22150@gmail.com>
2023-08-18 14:22:36 +0200

    structure for language files: 1 file by language, 1 group by vue component

---
+++ src/locales/deutsch.js
@@ -0,0 +1,38 @@
+export const messages = {
+  de: {
+    networkMap: {
+      family: "Familie",
+      friends: "Freund*innen/Bekannte",
+      colleagues: "Kolleg*innen",
+      professionals: "prof. Helfer*innen",
+    },
+    sideMenu: {
+      newNWK: "Neue NWK",
+      open: "Öffnen",
+    },
+    statisticPanel: {
+      msg: "",
+    },
+    viewOptions: {
+      msg: "",
+    },
+    pdfView: {
+      msg: "",
+    },
+    egoPanel: {
+      msg: "",
+    },
+    egoEdit: {
+      msg: "",
+    },
+    alterPanel: {
+      msg: "",
+    },
+    alterEdit: {
+      msg: "",
+    },
+    alterConnections: {
+      msg: "",
+    },
+  },
+};
---
+++ src/locales/english.js
@@ -0,0 +1,38 @@
+export const messages = {
+  de: {
+    networkMap: {
+      family: "Family",
+      friends: "Friends",
+      colleagues: "Colleagues",
+      professionals: "prof. Help",
+    },
+    sideMenu: {
+      newNWK: "New NWK",
+      open: "Open",
+    },
+    statisticPanel: {
+      msg: "",
+    },
+    viewOptions: {
+      msg: "",
+    },
+    pdfView: {
+      msg: "",
+    },
+    egoPanel: {
+      msg: "",
+    },
+    egoEdit: {
+      msg: "",
+    },
+    alterPanel: {
+      msg: "",
+    },
+    alterEdit: {
+      msg: "",
+    },
+    alterConnections: {
+      msg: "",
+    },
+  },
+};
