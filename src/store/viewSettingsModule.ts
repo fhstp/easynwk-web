f6523ccb1dcca2dd463c52ab1b465d900b8515b0
Alexander Rind <1217527+alex-rind@users.noreply.github.com>
2024-06-21 10:53:03 +0200

    remove session state from view option settings

--- src/store/viewSettingsModule.ts
+++ src/store/viewSettingsModule.ts
@@ -1,22 +1,13 @@
-export const TAB_BASE = "base";
-export const TAB_CONNECTIONS = "connections";
-
 export interface ViewOptionsFlags {
   horizons: boolean;
   connections: boolean;
   alteriNames: boolean;
-  statistics: boolean;
   ageInNwk: boolean;
   roleInNwk: boolean;
-  nwkcomparison: boolean;
-  nwkchange: boolean;
 }
 
 export interface ViewOptionsState extends ViewOptionsFlags {
   labelSizeInNwk: number;
-  selected: Set<number>;
-  editIndex: number | null;
-  editTab: string;
 }
 
 export function initViewOptionsState(): ViewOptionsState {
@@ -25,27 +16,16 @@ export function initViewOptionsState(): ViewOptionsState {
     horizons: false,
     connections: true,
     alteriNames: true,
-    statistics: false,
     ageInNwk: false,
     roleInNwk: false,
-    nwkcomparison: false,
-    nwkchange: false,
 
     labelSizeInNwk: 5,
-    selected: new Set<number>(),
-    editIndex: null,
-    editTab: "",
   };
 }
 
 const state = initViewOptionsState();
 
-const getters = {
-  isSelected:
-    (state: ViewOptionsState) =>
-    (alterId: number): boolean =>
-      state.selected.has(alterId),
-};
+const getters = {};
 
 const mutations = {
   updateFlag(
@@ -69,49 +49,6 @@ const mutations = {
 
   setLabelSizeInNwk(state: ViewOptionsState, newSize: number): void {
     state.labelSizeInNwk = newSize;
-  },
-
-  toggleAlterSelected(state: ViewOptionsState, alterId: number): void {
-    if (state.selected.has(alterId)) {
-      state.selected.delete(alterId);
-    } else {
-      state.selected.add(alterId);
-    }
-  },
-
-  selectSingleAlter(state: ViewOptionsState, alterId: number): void {
-    if (state.selected.has(alterId) && state.selected.size == 1) {
-      // exactly this alter was already selected
-      state.selected.clear();
-    } else {
-      state.selected.clear();
-      state.selected.add(alterId);
-    }
-  },
-
-  selectAlters(state: ViewOptionsState, alterIds: number[]): void {
-    state.selected = new Set(alterIds);
-  },
-
-  clearSelectedAlters(state: ViewOptionsState): void {
-    state.selected.clear();
-  },
-
-  openAlterForm(
-    state: ViewOptionsState,
-    payload: { alterIndex: number; tab?: string }
-  ): void {
-    state.editIndex = payload.alterIndex;
-    state.editTab = payload.tab ? payload.tab : TAB_BASE;
-
-    // intended side-effect: clearSelectedAlters
-    state.selected.clear();
-    // TODO nice to select opened alter (but index to id mapping missing)
-  },
-
-  closeAlterForm(state: ViewOptionsState): void {
-    state.editIndex = null;
-    state.editTab = "";
   },
 };
 
