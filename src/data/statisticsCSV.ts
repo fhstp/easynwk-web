4d9ca6140eaa5522a53593b1c3ad4c0a806aebc4
Eliasboss2006 <elias.patru@gmail.com>
2024-08-26 08:03:04 +0200

    ADD improvements

--- src/data/statisticsCSV.ts
+++ src/data/statisticsCSV.ts
@@ -13,47 +13,44 @@ import { NWKVersion } from "./NWKVersion";
 
 const SEP = ";";
 let output = "";
-let isFirstTime = true;
 
 export function statisticsCSV(
   versions: NWKVersion[]
 ): string {
+  output = "";
   
-  if (isFirstTime) {
-    output += "Name" + SEP;
-    output += "Datum" + SEP;
-    output += "ID" + SEP;
-    output += "Netzwerksektor" + SEP;
-    output += "Netzwerkgröße" + SEP;
-    output += "Netzwerkgröße (+aktivierbare)" + SEP;
-    output += "nach Geschlecht weiblich" + SEP;
-    output += "nach Geschlecht weiblich (+aktivierbare)" + SEP;
-    output += "nach Geschlecht männlich" + SEP;
-    output += "nach Geschlecht männlich (+aktivierbare)" + SEP;
-    output += "nach Geschlecht divers" + SEP;
-    output += "nach Geschlecht divers (+aktivierbare)" + SEP;
-    output += "nach Geschlecht nicht festgelegt" + SEP;
-    output += "nach Geschlecht nicht festgelegt (+aktivierbare)" + SEP;
-    output += "nach Horizont nah" + SEP;
-    output += "nach Horizont nah (+aktivierbare)" + SEP;
-    output += "nach Horizont mittel" + SEP;
-    output += "nach Horizont mittel (+aktivierbare)" + SEP;
-    output += "nach Horizont entfernt" + SEP;
-    output += "nach Horizont entfernt (+aktivierbare)" + SEP;
-    output += "Durschschn. Nähe" + SEP;
-    output += "Durschschn. Nähe (SD)" + SEP;
-    output += "Dichte" + SEP;
-    output += "Durchschn. Degree" + SEP;
-    output += "Durchschn. Degree (SD)" + SEP;
-    output += "Star(s)" + SEP;
-    output += "Isolierte" + SEP;
-    output += "Personen ohne Kante zum Ego" + SEP;
-    output += "\n";
-    isFirstTime = false;
-  }
+  output += "Name des Egos" + SEP;
+  output += "Name der Netzwerkkarte" + SEP;
+  output += "Datum" + SEP;
+  output += "ID" + SEP;
+  output += "Netzwerksektor" + SEP;
+  output += "Netzwerkgröße" + SEP;
+  output += "Netzwerkgröße (+aktivierbare)" + SEP;
+  output += "nach Geschlecht weiblich" + SEP;
+  output += "nach Geschlecht weiblich (+aktivierbare)" + SEP;
+  output += "nach Geschlecht männlich" + SEP;
+  output += "nach Geschlecht männlich (+aktivierbare)" + SEP;
+  output += "nach Geschlecht divers" + SEP;
+  output += "nach Geschlecht divers (+aktivierbare)" + SEP;
+  output += "nach Geschlecht nicht festgelegt" + SEP;
+  output += "nach Geschlecht nicht festgelegt (+aktivierbare)" + SEP;
+  output += "nach Horizont nah" + SEP;
+  output += "nach Horizont nah (+aktivierbare)" + SEP;
+  output += "nach Horizont mittel" + SEP;
+  output += "nach Horizont mittel (+aktivierbare)" + SEP;
+  output += "nach Horizont entfernt" + SEP;
+  output += "nach Horizont entfernt (+aktivierbare)" + SEP;
+  output += "Durschschn. Nähe" + SEP;
+  output += "Durschschn. Nähe (SD)" + SEP;
+  output += "Dichte" + SEP;
+  output += "Durchschn. Degree" + SEP;
+  output += "Durchschn. Degree (SD)" + SEP;
+  output += "Star(s)" + SEP;
+  output += "Isolierte" + SEP;
+  output += "Personen ohne Kante zum Ego" + SEP;
+  output += "\n";
 
   for (const version of versions) {
-    if (version.id === store.state.record.currentVersion) {
       for (const cat of allAlterCategorizationKeys) {
         const categorization = getAlterCategorization(cat);
         const networkAnalysisMap = analyseNWKbyCategory(version.nwk, categorization);
@@ -61,19 +58,19 @@ export function statisticsCSV(
         for (const label of categorization.categories) {
           const networkAnalysis = networkAnalysisMap.get(label);
           if (networkAnalysis) {
-            getDataForKeyFigures(networkAnalysis, version.title, version.date, version.id, categorization.label, label);
+            getDataForKeyFigures(networkAnalysis, version.nwk.ego.name,  version.title, version.date, version.id, categorization.label, label);
           }
           output += "\n";
         }
       }
-    }
   }
   
   output += "\n";
   return output;
 }
 
-function getDataForKeyFigures(networkAnalysis: NetworkAnalysis, title: string, date: string, id: number, categoryLabel: string, label: string) {
+function getDataForKeyFigures(networkAnalysis: NetworkAnalysis, ego: string,  title: string, date: string, id: number, categoryLabel: string, label: string) {
+  output += ego + SEP;
   output += title + SEP; 
   output += date + SEP;
   output += id + SEP;
